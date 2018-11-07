17ddeda738a9611440a86a90183b4e0fe585f470
kutselabskii <osutanfirst@gmail.com>
2018-11-07 19:35:08 +1000

    Add mister Skeltal

---
+++ Assets/skeltal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeltal : MonoBehaviour {
+
+    private bool keyPressed;
+
+    private int speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        keyPressed = false;
+        if (Input.GetKey("w"))
+        {
+            GetComponent<Animator>().Play("Run");
+            Vector3 mover = transform.forward;
+            mover = new Vector3(mover.x, mover.y);
+            GetComponent<Transform>().Translate(0, 0, speed * Time.deltaTime);
+            keyPressed = true;
+        }
+
+        if (!keyPressed)
+            GetComponent<Animator>().Play("Stand");
+
+            
+	}
+}
