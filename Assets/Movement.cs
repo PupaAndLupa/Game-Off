429513802343ae08d3dd2017d1d8709143d6d37c
kutselabskii <osutanfirst@gmail.com>
2018-11-07 14:59:10 +1000

    Add simple topdown test scene

---
+++ Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int horizontal = 0, vertical = 0;
+
+        if (Input.GetKey("a"))
+            horizontal -= 1;
+            
+        if (Input.GetKey("d"))
+            horizontal += 1;
+
+        if (Input.GetKey("w"))
+            vertical += 1;
+
+        if (Input.GetKey("s"))
+            vertical -= 1;
+   
+
+        transform.Translate(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime, 0);
+    }
+}
