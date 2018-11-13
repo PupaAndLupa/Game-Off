6077b26bbdfecf0b6ce03a85df499633c9d45c69
kutselabskii <osutanfirst@gmail.com>
2018-11-13 18:27:58 +1000

    Add fps camera from tutorial

---
+++ Assets/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private readonly float minimumX = -60f;
+    private readonly float maximumX = 60f;
+    private readonly float minimumY = -360f;
+    private readonly float maximumY = 360f;
+
+    private readonly float sensitivityX = 15f;
+    private readonly float sensitivityY = 15f;
+
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+
+    public Camera cam;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        offset = cam.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        transform.localEulerAngles = new Vector3(0, rotationY);
+        cam.transform.localEulerAngles = new Vector3(-rotationX, rotationY);
+
+        if (Input.GetKey("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        cam.transform.position = transform.position - offset;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float walkSpeed = 100f;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        moveDirection = (horizontalInput * transform.right + verticalInput * transform.forward).normalized;
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 yComponent = new Vector3(0, rb.velocity.y);
+       rb.velocity = moveDirection * walkSpeed * Time.deltaTime + yComponent;
+    }
+}
