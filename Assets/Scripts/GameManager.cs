0b0641b9935b30c4d7b49418b5a19c7249460c7c
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2018-11-29 19:58:56 +1000

    [UI] Add pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,13 @@ public class GameManager : MonoBehaviour {
 
     public BoardManager BoardManager;
 
-	void Start () {
+    public GameObject PausePanel;
+
+	void Start ()
+    {
         BoardManager = GetComponent<BoardManager>();
+        PausePanel = GameObject.FindGameObjectWithTag("PauseMenuPanel");
+        PausePanel.SetActive(false);
         InitGame();
 	}
 	
@@ -16,7 +21,30 @@ public class GameManager : MonoBehaviour {
         BoardManager.SetupScene();
     }
 
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!PausePanel.activeInHierarchy)
+            {
+                PauseGame();
+            } else if (PausePanel.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    }
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+        //enable the scripts again
+    }
 }
---
+++ Assets/Scripts/ResumeGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ResumeGame : MonoBehaviour {
+    [SerializeField]
+    private GameObject pausePanel;
+
+    public void Resume()
+    {
+        if (pausePanel.activeInHierarchy)
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+}
