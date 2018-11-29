82b4ca3c9f4d25d21093b2c48efe3b690251c624
Alexander Gomeniuk <gomeniuk.aa@gmail.com>
2018-11-29 17:18:42 +1000

    [MainMenu] Add ability to quit game; Add ability to change settings

---
+++ Assets/Scripts/SwitchPanel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SwitchPanel : MonoBehaviour {
+    public GameObject Current;
+    public GameObject ToSwitch;
+
+    public void Switch()
+    {
+        ToSwitch.SetActive(true);
+        Current.SetActive(false);
+    }
+}
