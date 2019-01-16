cdbfc68674f773c401a3b08a8006c5fc59d9a749
kutselabskii <osutanfirst@gmail.com>
2019-01-16 15:39:50 +1000

    Fix rotation on player's death

---
+++ Assets/Scripts/Actors/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Actor
+{
+    public override void Die()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Animator>().enabled = false;
+
+        transform.Find("Camera").rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        
+        IsTotallyDead = true;
+    }
+}
